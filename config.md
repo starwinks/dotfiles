26a416a5f8bf5f57afc5327e884817abcc8fd280
starwink <241220117@smail.nju.edu.cn>
2026-08-26 13:29:30 +0800

    feat: add powershell, git

---
+++ config.md
@@ -0,0 +1,10 @@
+## 1. powershell
+/usr/local/bin/powershell -> /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe
+```zsh
+sudo ln -s /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe /usr/local/bin/powershell
+```
+## 2. clip
+/usr/local/bin/clip -> /mnt/c/Windows/System32/clip.exe
+```zsh
+sudo ln -s /mnt/c/Windows/System32/clip.exe /usr/local/bin/clip
+```
--- zsh/modules/alias.zsh
+++ zsh/modules/alias.zsh
@@ -15,8 +15,6 @@ alias l='ls -CF'
 # Proxy control
 alias proxy_off='unset http_proxy https_proxy all_proxy HTTPS_PROXY HTTP_PROXY ALL_PROXY no_proxy NO_PROXY'
 
-# Cloudflare tunnel
-alias starttunnel='cloudflared tunnel --config ~/.cloudflared/config.yml run starserver'
 
 # EasyConnect VPN container
 alias ec='docker run -d --device /dev/net/tun --cap-add NET_ADMIN -ti -p 127.0.0.1:1080:1080 -p 127.0.0.1:8888:8888 -v /tmp/.X11-unix:/tmp/.X11-unix -e EC_VER=7.6.7 -e DISPLAY hagb/docker-easyconnect:vncless-7.6.7'
@@ -24,3 +22,11 @@ alias eckill='docker ps -q --filter ancestor=hagb/docker-easyconnect:vncless-7.6
 
 # Markdown to PDF
 alias md2pdf='md2pdf --config-file ~/tools/md2pdf/config.js'
+
+# cc-switch
+alias ccc=cc-switch-cli
+alias ccu='cc-switch > /dev/null 2>&1 &'
+
+alias format='clang-format-20'
+alias clang-format='clang-format-20'
+alias llama='/home/starwink/llama.cpp/build/bin/llama-server'
